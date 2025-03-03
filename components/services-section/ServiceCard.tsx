2ea4bac67ac42541df1fbf4bedb96d4eafc5b105
Talha Khan <talha7k@gmail.com>
2025-03-03 12:12:27 +0300

    Fixed headers of services card.

--- components/services-section/ServiceCard.tsx
+++ components/services-section/ServiceCard.tsx
@@ -11,18 +11,18 @@ interface ServiceCardProps {
 export function ServiceCard({ title, icon, subservices }: ServiceCardProps) {
   return (
     <div className="bg-transparent/20 rounded-xl shadow-md overflow-hidden mb-4">
-      <div className="px-2 pb-2">
+      <div className="px-2 pb-2 ">
         <div className="flex flex-col items-center align-middle justify-center mb-2">
-          <div className="p-5 items-center rounded-xl bg-white/5">
+          <div className="p-3 py-6 items-center px-[100%] bg-white/5">
             {/* Icon Container - Centered and sized */}
-            <div className="relative rounded-full mx-auto">
+            <div className="relativerounded-full mx-auto">
               <div className="flex items-center justify-center mb-3">
                 <Image
                   src={icon}
                   alt={`${title} icon`}
                   width={40}
                   height={40}
-                  className="invert-[60%]"
+                  className="invert-[70%]"
                   style={{
                     maxWidth: '80%',
                     maxHeight: '80%',
@@ -30,7 +30,7 @@ export function ServiceCard({ title, icon, subservices }: ServiceCardProps) {
                 />
               </div>
             </div>
-            <h2 className="text-2xl font-bold text-center text-gray-500">{title}</h2>
+            <h2 className="text-2xl w-[300px]  font-bold text-center text-gray-500">{title}</h2>
           </div>
         </div>
 
